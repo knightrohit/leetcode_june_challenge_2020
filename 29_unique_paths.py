978c33c570b5de3c3f1ea0a06a279eccbf756d3c
Rohit <knight.rohit@gmail.com>
2020-06-30 23:29:06 -0500

    using dp

--- 29_unique_paths.py
+++ 29_unique_paths.py
@@ -19,3 +19,23 @@ class Solution:
             return dfs(row, col+1) + dfs(row+1, col)
 
         return dfs()
+
+
+
+#DP - Botton up approach
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        
+        memo = [[0]*n for _ in range(m)]
+        
+        for i in range(m):
+            memo[i][n-1] = 1
+        
+        for j in range(n):
+            memo[m-1][j] = 1
+            
+        for row in range(m-2, -1, -1):
+            for col in range(n-2, -1, -1):                
+                memo[row][col] = memo[row][col+1] + memo[row+1][col]
+                
+        return memo[0][0]
